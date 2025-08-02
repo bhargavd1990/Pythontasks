28cd59b0dae6ee6f3b41431d45c13b5ed258aa3a
bhargavd1990 <bhargav.dyn@gmail.com>
2025-08-02 20:48:55 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,15 @@
+# Taking user inputs
+num1 = 5
+num2 = 10
+
+# Performing operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Displaying results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
---
+++ Personalised Greeting.py
@@ -0,0 +1,6 @@
+first_name = "John"
+last_name = "Doe"
+
+full_name = first_name + " " + last_name
+
+print("Hello, " + full_name + "! Welcome to the Python program.")
